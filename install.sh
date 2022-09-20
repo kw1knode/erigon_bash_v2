7d11968011e9526ff839edbd6af5f36be81197f3
kw1k <kw1k@nodeify.net>
2022-09-19 17:29:15 -0700

    Update
    use systemctl

--- install.sh
+++ install.sh
@@ -2,45 +2,63 @@
 apt update -y && apt upgrade -y && apt autoremove -y
 
 #Prerequisites
-sudo apt-get install -y build-essential supervisor wget git
-
+sudo apt-get install -y build-essential
 
 
 
 #Golang
-wget https://golang.org/dl/go1.18.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
-sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
+cd ~
+curl -LO https://go.dev/dl/go1.19.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+source $HOME/.profile
+rm go1.19.linux-amd64.tar.gz
 
-sudo useradd -m -s /bin/bash erigon
+#Erigon
+cd ~
+curl -LO https://github.com/ledgerwatch/erigon/archive/refs/tags/v2022.09.02.tar.gz
+tar xvf v2022.09.02.tar.gz
+cd erigon-2022.09.02
+make erigon
+cd ~
+sudo cp -a erigon-2022.09.02 /usr/local/bin/erigon
+rm v2022.09.02.tar.gz
+rm -r erigon-2022.09.01
+sudo useradd --no-create-home --shell /bin/false erigon
+sudo mkdir -p /var/lib/erigon
+sudo chown -R erigon:erigon /var/lib/erigon
 
-cd /opt
-sudo mkdir erigon
-sudo mkdir github && cd github
-sudo git clone https://github.com/ledgerwatch/erigon.git
-cd erigon
-git checkout v2022.08.02
-sudo make
-sudo cp -r ./build/bin /opt/erigon/
-sudo mkdir -p /data/erigon/datadir
-sudo chown -R erigon:erigon /data/erigon
+echo "[Unit]
+Description=Erigon Execution Client (Ethereum Main Network)
+After=network.target
+Wants=network.target
+[Service]
+User=erigon
+Group=erigon
+Type=simple
+Restart=always
+RestartSec=5
+ExecStart=/usr/local/bin/erigon/build/bin/erigon \
+  --datadir=/var/lib/erigon \
+  --http.api=engine,eth,net \
+  --rpc.gascap=50000000 \
+  --http \
+  --ws \
+  --rpc.batch.concurrency=100 \
+  --state.cache=2000000 \
+  --http.addr="0.0.0.0" \
+  --http.port=8545 \
+  --http.api="eth,erigon,web3,net,debug,trace,txpool" \
+  --private.api.addr="0.0.0.0:9595" \
+  --http.corsdomain="*" \
+  --torrent.download.rate 90m \
+  --authrpc.jwtsecret=/var/lib/jwtsecret/jwt.hex \
+  --private.api.addr= \
+  --metrics 
+[Install]
+WantedBy=default.target" >> /etc/systemd/system/erigon.service \
 
-echo  "[program:erigon]
-command=bash -c '/opt/erigon/bin/erigon --datadir="/data/erigon/datadir" --rpc.gascap=50000000 --rpc.batch.concurrency=100 --http.addr="0.0.0.0" --http.port=8545 --private.api.addr="0.0.0.0:9595" --http.corsdomain="*" --http.api="eth,erigon,web3,net,debug,trace,txpool" --torrent.download.rate 90m --state.cache=2000000 --http --ws --authrpc.jwtsecret=/data/erigon/datadir/jwt.hex --metrics'
-user=erigon
-autostart=true
-autorestart=true
-stderr_logfile=/var/log/supervisor/erigon.err.log
-stderr_logfile_maxbytes=1000000
-stderr_logfile_backups=10
-stdout_logfile=/var/log/supervisor/erigon.out.log
-stdout_logfile_maxbytes=1000000
-stdout_logfile_backups=10
-stopwaitsecs=300
-" >> /etc/supervisor/conf.d/erigon.conf \
 
-sudo systemctl enable supervisor
-sudo systemctl start supervisor
-sudo supervisorctl update
-sleep 5
-supervisorctl status erigon
+sudo systemctl daemon-reload
+sudo systemctl start erigon
